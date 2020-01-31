e412dd99e397a82dba82960f5cf67a4e98d34e21
timurlaura729 <53204973+timurlaura729@users.noreply.github.com>
2020-01-31 12:49:37 +0600

    Изменил структуру кейсов 2

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
                                     </div>
                                     <div class="col-md-6">
                                         <div class="control">
-                                            <input type="text" class="control__input" id="contphone2" class="phone_mask" oninput="proverkaPress(document.getElementById('contphone2'), document.getElementById('but2'))" placeholder="Номер телефона">
+                                            <input type="text" class="control__input" id="contphone2" class="phone_mask" oninput="proverkaPress(document.getElementById('contphone2'), document.getElementById('but2'))" placeholder="Номер телефона1">
                                         </div>
                                     </div>
                                     <div class="col-md-6">
