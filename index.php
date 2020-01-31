626605e921a4d5a5e2d8c0f69b09ec6858f6dcdb
timurlaura729 <53204973+timurlaura729@users.noreply.github.com>
2020-01-31 15:04:05 +0600

    Изменил структуру кейсов 2

--- index.php
+++ index.php
@@ -1302,8 +1302,7 @@
 
             function proverkaPress(element, element2) {
                 telPress=ph(element.value);
-                element.value[0]="+";
-                element.value[1]="7";
+                alert(element.value[0]);
                 if(telPress==1) element2.disabled = false; else element2.disabled = true;
             }
 
