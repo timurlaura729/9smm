cfd3bc07396182459cc6129f8574cbd35d202c23
timurlaura729 <53204973+timurlaura729@users.noreply.github.com>
2020-02-03 17:41:05 +0600

    up

--- index.php
+++ index.php
@@ -1433,7 +1433,7 @@
     <script src="static/js/separate-js/scripts.js"></script>
     <script>
         $(document).ready(function(){
-            $("#contphone2").mask("+7(999) 999-99-99",{input:function(){ proverkaPress(document.getElementById('contphone2'), document.getElementById('but2')); alert("phone_mask"); }});
+            $("#contphone2").on('input', function() { proverkaPress(document.getElementById('contphone2'), document.getElementById('but2')); alert("phone_mask"); });
             $("#contphone2").mask("+7(999) 999-99-99",{completed:function(){ proverkaPress(document.getElementById('contphone2'), document.getElementById('but2')); }});
         })
     </script>
